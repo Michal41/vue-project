7e328ad7820223be3ff46e7b28df1f91656dd18f
michal <michal.kanarek@gmail.com>
2022-05-23 20:13:03 +0200

    CustomInput component

---
+++ src/components/CustomInput.vue
@@ -0,0 +1,21 @@
+<template>
+  <input class="customInput" @input="(e) => $emit('handleChange', e)" />
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+  .customInput {
+    display: block;
+    width: 270px;
+    margin-top: 2em;
+    padding: 1em;
+    background: #292015;
+    border-radius: 15px;
+    color: #F5F4F4;
+    border: 1px solid #707070;
+    outline: none;
+  }
+</style>
--- src/views/SignInView.vue
+++ src/views/SignInView.vue
@@ -1,11 +1,21 @@
 <template>
   <div :style="{backgroundImage:`url(${Background})`}" class="signInBackground">
-  <div></div>
+    <div class="inputContainer">
+      <label>
+        Podaj swój login
+      </label>
+      <custom-input @handle-change="handleChange"></custom-input>
+    </div>
   </div>
 </template>
 
 <script setup>
 import Background from '../assets/signInBackground.png'
+import CustomInput from '../components/CustomInput.vue'
+
+function handleChange(e) {
+  console.log(e.target.value)
+}
 </script>
 
 <style scoped>
@@ -14,5 +24,17 @@ import Background from '../assets/signInBackground.png'
     height: 100vh;
     background-repeat: no-repeat;
     background-size: cover;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  .inputContainer {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  .inputContainer label{
+    font-size: 25px;
+    font-weight: bold;
   }
 </style>
